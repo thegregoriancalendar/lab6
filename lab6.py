b58fb807b75912c4328579e614bed8b9f6705bf9
thegregoriancalendar <89663385+thegregoriancalendar@users.noreply.github.com>
2024-10-20 20:08:25 -0400

    comments and stuff
    added comments and changed some of the inputs to look nicer

--- lab6.py
+++ lab6.py
@@ -1,21 +1,27 @@
+# Gregory Roudenko
+
 quit = False
 
 def encode(string):
+    # loops through chars in string
     for i in range(len(string)):
         newVal = int(string[i]) + 3
+        # wraparound check
         if newVal >= 10:
             newVal -= 10
+        # changes the char
         string = string[:i] + str(newVal) + string[i + 1:]
     return string
         
 def main():
-    choice = input("Choose a function (encode or decode) or enter 'quit' to quit: ")
+    choice = input("Choose a function (encode or decode) or enter \"quit\" to quit: ")
     if choice == "quit":
         return True
     val = input("Choose a value to " + choice + ": ")
+    # evaluates whatever function you enter into the prompt
     print(eval(choice + "(\"" + val + "\")"))
 
-
+# loop
 if __name__ == "__main__":
     while (not quit):
         quit = main()
