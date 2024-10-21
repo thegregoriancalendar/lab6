db57fdcd937a42d8ae8e2ecb007d0d84346b9051
Melissa Cote <diamondmeadows22@gmail.com>
2024-10-21 17:27:45 -0400

    added decoded() function and comments

--- lab6.py
+++ lab6.py
@@ -17,6 +17,19 @@ def encode(string):
     print("Your password has been encoded and stored!")
 
     return string
+
+
+def decode(encoded):
+    decoded = "" # initialize empty string for result to be returned
+    # Loops through each digit in the encoded password, changes it to an integer, and subtracts 3. If the new digit is
+    # less than 0, adds 10 to account for encoded values that would have been greater than 10 when increased by 3. Each
+    # new digit is then changed to a string and added to the decoded string, which is returned when completed.
+    for digit in encoded:
+        temp_digit = int(digit) - 3
+        if temp_digit < 0:
+            temp_digit += 10
+        decoded += str(temp_digit)
+    print(f"The encoded password is {encoded}, and the original password is {decoded}.")
         
 def main():
     choice = input("Choose a function (encode or decode) or enter \"quit\" to quit: ")
